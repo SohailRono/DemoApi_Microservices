b4a37b9e5348627574af20d64f957653e14da5d2
Rana IT <rana.it@echotex.com>
2023-05-28 17:13:28 +0600

    customerrepos

---
+++ src/Services/DemoNet.Api/Models/Entities/CustomerInfo.cs
@@ -0,0 +1,33 @@
+﻿using DemoNet.Api.Models.Common;
+
+namespace DemoNet.Api.Models.Entities
+{
+    public class CustomerInfo : EntityBase
+    {
+        public string CustId { get; set; }
+
+        public string CustName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? BnName { get; set; }
+
+        public bool? IsActive { get; set; } = false;
+
+        public string? PackCode { get; set; }
+
+        public decimal? BillAmt { get; set; }
+
+        public string? Line { get; set; }
+
+        public string? Ip { get; set; }
+
+        public string? AreaCode { get; set; }
+
+        public string? Remark { get; set; }
+    }
+}
---
+++ src/Services/DemoNet.Api/Repositories/CustomerInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using DemoNet.Api.Data;
+using DemoNet.Api.Interfaces;
+using DemoNet.Api.Models.Entities;
+
+namespace DemoNet.Api.Repositories
+{
+    public class CustomerInfoRepository : RepositoryBase<CustomerInfo>, ICustomerInfoRepository
+    {
+        public CustomerInfoRepository(DemoDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
