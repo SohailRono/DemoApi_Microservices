feb641cebe617043900906260b0849ee33435c6b
Rana IT <rana.it@echotex.com>
2023-05-29 15:11:28 +0600

    Revert "Revert "customerrepos""
    This reverts commit fece00a6774e0bc1b1e0a8fc9faffbfa1dc22a64.

---
+++ src/Services/DemoNet.Api/Data/DemoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DemoNet.Api.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoNet.Api.Data
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options) : base(options) { }
+
+        public DbSet<CustomerInfo> CustomerInfo { get; set; }
+    }
+}
