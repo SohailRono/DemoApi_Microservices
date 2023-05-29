f473251aaa18d75214ec33006d36976832186676
Rana IT <rana.it@echotex.com>
2023-05-29 15:28:03 +0600

    vwmodel

---
+++ src/Services/DemoNet.Api/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DemoNet.Api.Models.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DemoNet.Api.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CustomerInfo, VmCustomerInfo>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/DemoNet.Api/Models/VwModels/VmCustomerInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoNet.Api.Models.VwModels
+{
+    public class VmCustomerInfo
+    {
+        public int Id { get; set; }
+        public string CustId { get; set; }
+
+        public string CustName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? BnName { get; set; }
+    }
+}
