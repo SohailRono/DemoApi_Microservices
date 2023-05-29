6d04bf0a4726e8ed84599efae8126ae119926d58
Rana IT <rana.it@echotex.com>
2023-05-29 13:13:20 +0600

    Revert "Revert "customerrepos""
    This reverts commit fece00a6774e0bc1b1e0a8fc9faffbfa1dc22a64.

---
+++ src/Services/DemoNet.Api/Interfaces/ICustomerInfoRepository.cs
@@ -0,0 +1,9 @@
+﻿using DemoNet.Api.Interfaces;
+using DemoNet.Api.Models.Entities;
+
+namespace DemoNet.Api.Interfaces
+{
+    public interface ICustomerInfoRepository : IAsyncRepository<CustomerInfo> 
+    {
+    }
+}
